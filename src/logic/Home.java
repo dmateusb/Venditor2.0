ef7c4f5e7f122e0a35604fd96d2bd602da66b040
David Mateus <dmateusb@unal.edu.co>
2020-05-06 18:24:48 -0500

    Commit David 2

--- src/logic/Home.java
+++ src/logic/Home.java
@@ -46,7 +46,7 @@ public class Home extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/Login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/Home.fxml"));
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(root);
         stage.setScene(scene);
